778207f0568d396b550a4149330510da6e942943
lucas <lucas.007nick@gmail.com>
2026-05-20 18:10:40 -0300

    corrigido erro de sintaxe

--- terraform2/main.tf
+++ terraform2/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_container_registry" "acr" {
     name = var.acr_name
-    resource_group_name = var.resource_group.rg.name
+    resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
     sku = "Standard"
     admin_enabled = true
