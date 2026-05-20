08371b26fcd8cc9aa82e4faa0ea55cb4b61dc520
lucas <lucas.007nick@gmail.com>
2026-05-20 18:04:54 -0300

    teste modificação da linha 8

--- terraform2/main.tf
+++ terraform2/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_container_registry" "acr" {
     name = var.acr_name
-    resource_group_name = var.resource_group.rg.name
+    resource_group_name = var.resource_group_name.name
     location = azurerm_resource_group.rg.location
     sku = "Standard"
     admin_enabled = true
